028e449b0b23d6efad6aaf03b4824c4199982028
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2024-03-21 09:52:39 +1000

    add user policy and spec

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,19 @@
+class UserPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+    def show?
+      true # Anyone can view user profiles
+    end
+  
+    def update?
+      user == record
+    end
+  
+    def destroy?
+      user.admin? || user == record
+    end
+  end
+end
---
+++ spec/policies/user_policy_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe UserPolicy, type: :policy do
+  let(:regular_user) { build(:user) }  # Or use create(:user) if needed
+  let(:admin_user) { build(:user, admin: true) }
+
+  # subject { described_class.new(user, User) } # User to be authorized
+
+  # context "for a regular user" do
+  #   let(:user) { regular_user }
+
+  #   it { should_not permit_action(:update) }  
+  #   it { should_not permit_action(:destroy) }
+  # end
+
+  # context "for an admin user" do
+  #   let(:user) { admin_user }
+
+  #   it { should permit_action(:update) } 
+  #   it { should permit_action(:destroy) }
+  # end
+
+  permissions ".scope" do
+    pending "add some examples to (or delete) #{__FILE__}"
+  end
+
+  permissions :show? do
+    pending "add some examples to (or delete) #{__FILE__}"
+  end
+
+  permissions :create? do
+    pending "add some examples to (or delete) #{__FILE__}"
+  end
+
+  permissions :update? do
+    pending "add some examples to (or delete) #{__FILE__}"
+  end
+
+  permissions :destroy? do
+    pending "add some examples to (or delete) #{__FILE__}"
+  end
+end
