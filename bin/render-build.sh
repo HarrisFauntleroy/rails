41a780fa25f0a2d04c5716ff4e2acb2d51b5a416
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2024-04-21 21:33:12 +1000

    render changes

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -10,6 +10,6 @@ bundle exec rake assets:precompile
 bundle exec rake assets:clean
 
 # For a fresh setup, you can run the following commands:
-# rails db:drop db:create db:schema:load db:migrate db:seed # For initial setup only
+rails db:drop db:create db:schema:load db:migrate db:seed 
 
-bundle exec rake db:migrate
+# bundle exec rake db:migrate
