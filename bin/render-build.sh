aa4415582026c28079615304b4d01bda95fe3ad5
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2024-04-21 21:23:08 +1000

    improve UI

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,5 +8,8 @@ bundle config --local build.nokogiri --use-system-libraries
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-# bundle exec rake db:schema:load db:seed # For initial setup only
+
+# For a fresh setup, you can run the following commands:
+# rails db:drop db:create db:schema:load db:migrate db:seed # For initial setup only
+
 bundle exec rake db:migrate
