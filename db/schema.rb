00ab4bb6822da8e2a453adef7822727ed89753da
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2024-04-25 11:43:46 +1000

    Update schema.rb to use a consistent date format

--- db/schema.rb
+++ db/schema.rb
@@ -10,94 +10,94 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_420_223_844) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_20_223844) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'articles', force: :cascade do |t|
-    t.string 'title'
-    t.text 'body'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'categories', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'user_id', default: 1, null: false
-    t.index ['user_id'], name: 'index_categories_on_user_id'
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", default: 1, null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
-  create_table 'comments', force: :cascade do |t|
-    t.text 'content'
-    t.integer 'topic_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'user_id'
-    t.integer 'parent_comment_id'
-    t.index ['topic_id'], name: 'index_comments_on_topic_id'
-    t.index ['user_id'], name: 'index_comments_on_user_id'
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "topic_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "parent_comment_id"
+    t.index ["topic_id"], name: "index_comments_on_topic_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table 'forums', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'user_id', null: false
-    t.string 'description'
-    t.bigint 'category_id', default: 1, null: false
-    t.index ['category_id'], name: 'index_forums_on_category_id'
-    t.index ['user_id'], name: 'index_forums_on_user_id'
+  create_table "forums", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.string "description"
+    t.bigint "category_id", default: 1, null: false
+    t.index ["category_id"], name: "index_forums_on_category_id"
+    t.index ["user_id"], name: "index_forums_on_user_id"
   end
 
-  create_table 'news', force: :cascade do |t|
-    t.string 'title'
-    t.text 'body'
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_news_on_user_id'
+  create_table "news", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_news_on_user_id"
   end
 
-  create_table 'topics', force: :cascade do |t|
-    t.string 'title'
-    t.text 'content'
-    t.integer 'forum_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'user_id'
-    t.boolean 'sticky'
-    t.boolean 'announcement'
-    t.index ['forum_id'], name: 'index_topics_on_forum_id'
-    t.index ['user_id'], name: 'index_topics_on_user_id'
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "forum_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.boolean "sticky"
+    t.boolean "announcement"
+    t.index ["forum_id"], name: "index_topics_on_forum_id"
+    t.index ["user_id"], name: "index_topics_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.boolean 'admin'
-    t.string 'username'
-    t.string 'timezone'
-    t.float 'latitude'
-    t.float 'longitude'
-    t.datetime 'last_seen_at'
-    t.date 'date_of_birth'
-    t.string 'signature'
-    t.boolean 'moderator'
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "admin"
+    t.string "username"
+    t.string "timezone"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "last_seen_at"
+    t.date "date_of_birth"
+    t.string "signature"
+    t.boolean "moderator"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'categories', 'users'
-  add_foreign_key 'comments', 'topics'
-  add_foreign_key 'forums', 'categories'
-  add_foreign_key 'forums', 'users'
-  add_foreign_key 'news', 'users'
-  add_foreign_key 'topics', 'forums'
+  add_foreign_key "categories", "users"
+  add_foreign_key "comments", "topics"
+  add_foreign_key "forums", "categories"
+  add_foreign_key "forums", "users"
+  add_foreign_key "news", "users"
+  add_foreign_key "topics", "forums"
 end
