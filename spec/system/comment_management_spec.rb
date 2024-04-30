053a3f6695e019b821ed43f5c9053ce814d30db8
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2024-04-30 22:48:10 +1000

    Add comment management system tests

---
+++ spec/system/comment_management_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "CommentManagement", type: :system, js: true do
+  let(:user) { create(:user, id: 1) }
+  let(:forum) { create(:forum, user: user) }
+  let(:topic) { create(:topic, forum: forum, user: user) }
+  let(:comment) { create(:comment, topic: topic, user: user) }
+
+  before do
+    # driven_by(:selenium_chrome) # for JS testing
+    driven_by(:selenium_chrome_headless) # for headless js testing
+  end
+
+  it "initializes EasyMDE on the new topic form" do
+    sign_in user
+    visit new_forum_topic_path(forum, topic)
+
+    expect(page).to have_selector('#markdown-editor', visible: false)
+    expect(page).to have_css('.EasyMDEContainer', visible: true)
+  end
+
+  it 'initializes EasyMDE on the comment form' do
+    sign_in user
+    visit forum_topic_path(forum, topic)
+
+    expect(page).to have_selector('#markdown-editor', visible: false)
+    expect(page).to have_css('.EasyMDEContainer', visible: true)
+  end
+end
