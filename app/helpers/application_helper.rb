933590e722362aa9ab4642f0ca2157b88337f3f7
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2024-04-30 15:55:54 +1000

    add markdown rendering in ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,32 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  include Pundit::Authorization
+  require 'redcarpet'
+  require 'rouge'
+  require 'rouge/plugins/redcarpet'
+
+  class HTMLWithRouge < Redcarpet::Render::HTML
+    include Rouge::Plugins::Redcarpet
+  end
+
+  def markdown(text)
+    renderer = HTMLWithRouge.new(hard_wrap: true, filter_html: true)
+
+    extensions = {
+      autolink: true,
+      no_intra_emphasis: true,
+      fenced_code_blocks: true,
+      lax_spacing: true,
+      strikethrough: true,
+      superscript: true,
+      tables: true,
+      underline: true,
+      highlight: true,
+      quote: true,
+      footnotes: true
+    }
+
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+    markdown.render(text).html_safe
+  end
 end
