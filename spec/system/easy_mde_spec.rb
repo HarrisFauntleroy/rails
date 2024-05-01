83d26b774825d21cd42d158728b7f61ce708d374
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2024-05-01 13:15:26 +1000

    Add EasyMDE and MathJax integration system tests

---
+++ spec/system/easy_mde_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+# This spec tests the EasyMDE integration in the application.
+# EasyMDE is a CDN-hosted Markdown editor.
+
+RSpec.describe 'CommentManagement', type: :system, js: true do
+  let(:user) { create(:user, id: 1) }
+  let(:forum) { create(:forum, user:) }
+  let(:topic) { create(:topic, forum:, user:) }
+  let(:comment) { create(:comment, topic:, user:) }
+
+  before do
+    # driven_by(:selenium_chrome) # for JS testing
+    driven_by(:selenium_chrome_headless) # for headless js testing
+  end
+
+  it 'initializes EasyMDE on the new topic form' do
+    sign_in user
+    visit new_forum_topic_path(forum, topic)
+
+    expect(page).to have_selector('#markdown-editor', visible: false)
+    expect(page).to have_css('.EasyMDEContainer', visible: true)
+  end
+
+  it 'initializes EasyMDE on the comment form' do
+    sign_in user
+    visit forum_topic_path(forum, topic)
+
+    expect(page).to have_selector('#markdown-editor', visible: false)
+    expect(page).to have_css('.EasyMDEContainer', visible: true)
+  end
+end
