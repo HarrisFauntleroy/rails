610889826f9c5d779e2892be7d9ca0a7f8a21fa9
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2024-05-01 13:15:05 +1000

    Refactor TopicHelper to remove toggle_sticky and toggle_announcement actions

--- app/helpers/topic_helper.rb
+++ app/helpers/topic_helper.rb
@@ -1,27 +1,3 @@
 module TopicHelper
-  def toggle_sticky
-    @topic = Topic.find(params[:id])
-    authorize @topic, :toggle_sticky? # Assuming 'toggle_sticky?' policy exists
 
-    if @topic.sticky?
-      @topic.unmark_as_sticky!
-    else
-      @topic.mark_as_sticky!
-    end
-
-    redirect_to [@topic.forum, @topic], notice: 'Topic stickiness updated.'
-  end
-
-  def toggle_announcement
-    @topic = Topic.find(params[:id])
-    authorize @topic, :toggle_announcement? # Assuming 'toggle_announcement?' policy exists
-
-    if @topic.announcement?
-      @topic.unmark_as_announcement!
-    else
-      @topic.mark_as_announcement!
-    end
-
-    redirect_to [@topic.forum, @topic], notice: 'Topic stickiness updated.'
-  end
 end
