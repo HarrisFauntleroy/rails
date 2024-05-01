a36edd94d9a4338459e785c9355c4d9b4aed51ec
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2024-05-01 13:14:28 +1000

    Refactor TopicsController to remove toggle_sticky and toggle_announcement actions

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -58,32 +58,6 @@ class TopicsController < ApplicationController
     redirect_to forum_path(@forum), notice: 'Topic deleted!'
   end
 
-  def toggle_sticky
-    @topic = Topic.find(params[:id])
-    authorize @topic, :toggle_sticky? # Assuming 'toggle_sticky?' policy exists
-
-    if @topic.sticky?
-      @topic.unmark_as_sticky!
-    else
-      @topic.mark_as_sticky!
-    end
-
-    redirect_to [@forum, @topic], notice: 'Topic stickiness updated.'
-  end
-
-  def toggle_announcement
-    @topic = Topic.find(params[:id])
-    authorize @topic, :toggle_announcement? # Assuming 'toggle_announcement?' policy exists
-
-    if @topic.announcement?
-      @topic.unmark_as_announcement!
-    else
-      @topic.mark_as_announcement!
-    end
-
-    redirect_to [@forum, @topic], notice: 'Topic stickiness updated.'
-  end
-
   private
 
   def set_forum
