4c20a66c5783f74eb617283c83c1bf3e9d431635
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2024-06-02 22:53:20 +1000

    chore: Add admin category creation and management feature specs

--- spec/features/category_flow_spec.rb
+++ spec/features/category_flow_spec.rb
@@ -6,6 +6,7 @@ describe 'Category Flow', js: true do
   let(:admin) { create(:user, :admin, id: 1) }
 
   it 'admin creates a category' do
+    # Admin logs in
     visit root_path
 
     within('#new_session') do
@@ -14,6 +15,7 @@ describe 'Category Flow', js: true do
       click_button 'Log in'
     end
 
+    # Admin creates a category
     find('#forums_link').click
     find('#new_category_link').click
 
@@ -22,11 +24,32 @@ describe 'Category Flow', js: true do
       click_button 'Submit'
     end
 
+    # Admin sees the category
     expect(page).to have_text('Testing Category')
     expect(page).to have_text('Forums')
     expect(page).to have_text('Topics')
     expect(page).to have_text('Replies')
     expect(page).to have_text('Last Comment')
-    expect(page).to have_text('Actions') 
+    expect(page).to have_text('Actions')
+
+    # Admin edits the category
+    find('#edit_category_link').click
+
+    within('#edit_category') do
+      fill_in 'category_name', with: 'New Category Name'
+      click_button 'Submit'
+    end
+
+    # Admin sees the updated category
+    expect(page).to have_text('New Category Name')
+
+    # Admin deletes the category
+    reload_page # TODO: reload should not be necessary, the button needs fixing
+
+    accept_confirm do
+      find('#delete_category_button').click
+    end
+
+    expect(page).not_to have_text('New Category Name')
   end
 end
