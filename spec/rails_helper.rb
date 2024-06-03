b1f3db7e5cfee8b0834afecdfee4133a27617d97
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2024-06-03 22:14:09 +1000

    chore: Update Capybara javascript driver configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,6 +64,14 @@ RSpec.configure do |config|
 
   config.include Warden::Test::Helpers
 
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium_chrome_headless
+  end
+
   config.after do
     Warden.test_reset!
   end
@@ -86,7 +94,7 @@ end
 Capybara.register_driver :headless_chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
   options.add_argument('--headless')
-  options.add_argument('--disable-gpu') # Not necessary for Linux, but required for Windows
+  options.add_argument('--disable-gpu')
   options.add_argument('--window-size=1400,1400')
 
   Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
